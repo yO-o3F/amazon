90f923c74cac5827d8ea8346d60c4c7fe17ee707
yO-o3F <m.ramezani100@yahoo.com>
2025-06-02 20:27:38 +0330

    making total cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -46,7 +46,7 @@ products.forEach((product) => {
         Added
       </div>
 
-      <button class="add-to-cart-button button-primary js-add-to-cart" data-product-name="${product.name}">
+      <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
         Add to Cart
       </button>
     </div>
@@ -59,12 +59,12 @@ document.querySelector('.js-products-grid')
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click', () => {
-      const productName = button.dataset.productName;
+      const productId = button.dataset.productId;
 
       let matchingItem;
 
       cart.forEach((item) => {
-        if (productName === item.productName) {
+        if (productId === item.productId) {
           matchingItem = item;
         }
       });
@@ -73,10 +73,18 @@ document.querySelectorAll('.js-add-to-cart')
         matchingItem.quantity += 1;
       } else {
         cart.push({
-          productName: productName,
+          productId: productId,
           quantity: 1
         });
       }
-      console.log(cart);
+
+      let cartQuantity = 0;
+
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      });
+
+      document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
     });
   });
