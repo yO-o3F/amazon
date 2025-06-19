e5aba03635790552e4aa6961062a5d3f8dea0f98
yO-o3F <m.ramezani100@yahoo.com>
2025-06-20 00:00:13 +0330

    Save cart it localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,14 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart')) || [{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2
 }, {
   productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
   quantity: 1
 }];
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId, quantity) {
   let matchingItem;
@@ -23,6 +27,8 @@ export function addToCart(productId, quantity) {
       quantity
     });
   }
+
+  saveToStorage();
 }
 
 
@@ -36,4 +42,6 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
+
+  saveToStorage();
 }
